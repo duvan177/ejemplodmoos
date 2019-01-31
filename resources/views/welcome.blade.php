b90b5fe5912b1bb02f9c4c55313dd2ec140c2fc8
duvan177 <47201336+duvan177@users.noreply.github.com>
2019-01-31 01:14:09 -0500

    Update welcome.blade.php
    hola

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Su madre
+                    gracias
                 </div>
 
                 <div class="links">
